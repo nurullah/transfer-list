9879f777d63769e354139730ad009c07467b594a
Nurullah Sevinçtekin <nsevinctekin@gmail.com>
2024-09-10 11:08:34 +0300

    Test task is almost ready.

---
+++ src/components/CheckboxList.jsx
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types';
+
+// UI
+import styles from './CheckboxList.module.scss';
+
+function CheckboxList({
+  data,
+  type,
+  className,
+  onChange,
+  onSubmit,
+  ...props
+}) {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // callback
+    if (onSubmit) {
+      const data = new FormData(e.currentTarget);
+
+      // check title
+      if (! data.get('name')?.length) return;
+  
+      // add id
+      data.append('id', Date.now());
+      data.append('type', type);
+
+      // return to callback
+      onSubmit(Object.fromEntries(data));
+    }
+
+    e.currentTarget.reset();
+  }
+
+  const handleChange = (item, status) => {
+    if (onChange) {
+      onChange({
+        ...item,
+        checked: status
+      });
+    }
+  }
+
+  return (
+    <div {...props} className={[styles.self].concat(className).join(' ')}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input type="text" name="name" placeholder={`Add ${type} item...`}/>
+        <button type="submit">+</button>
+      </form>
+      <ul className={styles.list}>
+        {data.map((item, key) => 
+          <li key={key}>
+            <label>
+              <input 
+                type="checkbox"
+                value={item.name}
+                checked={item.checked}
+                onChange={e => handleChange(item, e.target.checked)} />
+              <span>{item.name}</span>
+            </label>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+CheckboxList.propTypes = {
+  data: PropTypes.array,
+  type: PropTypes.string,
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func
+}
+
+export default CheckboxList;
---
+++ src/components/TransferList.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+
+import defaultItems from '../data/transfer-items.json';
+import CheckboxList from './CheckboxList';
+
+// UI
+import styles from './TransferList.module.scss';
+
+export default function TransferList() {
+  const [data, setData] = useState(defaultItems);
+  const sourceItems = data.filter(item => item.type === 'source');
+  const targetItems = data.filter(item => item.type === 'target');
+  const checkedSourceItems = sourceItems.filter(i => i.checked);
+  const checkedTargetItems = targetItems.filter(i => i.checked);
+
+  const addItem = (item) => setData((prev) => [ ...prev, item ]);
+
+  const handleChange = (item) => {
+    setData(prev => prev.map(prevItem => {
+      if (prevItem.id === item.id) {
+        return item;
+      }
+
+      return prevItem;
+    }))
+  };
+
+  const _transferItems = (currentType, newType, isCheck) => {
+    
+    setData(prev => prev.map(prevItem => {
+      if (prevItem.type === currentType) {
+
+        // isCheck filter is enable
+        if (isCheck !== undefined) {
+          if (! prevItem.checked) return prevItem;
+        }
+
+        return {
+          ...prevItem,
+          type: newType,
+          checked: false
+        }
+      }
+
+      return prevItem;
+    }));
+  }
+
+  const handleTransferSourceItems = () => {
+    if (! checkedSourceItems.length) return;
+
+    // update
+    _transferItems('source', 'target', true);
+  }
+
+  const handleTransferTargetItems = () => {
+    if (! checkedTargetItems.length) return;
+
+    // update
+    _transferItems('target', 'source', true);
+  }
+
+  const handleTransferAllSourceItems = () => {
+    if (! sourceItems.length) return;
+
+    // update
+    _transferItems('source', 'target', undefined);
+  }
+
+  const handleTransferAllTargetItems = () => {
+    if (! targetItems.length) return;
+
+    // update
+    _transferItems('target', 'source', undefined);
+  }
+
+  return (
+    <div className={styles.self}>
+      <CheckboxList 
+        type="source" 
+        data={sourceItems} 
+        className={styles.source}
+        onChange={handleChange}
+        onSubmit={addItem} />
+      <div className={styles.switch}>
+        <button 
+          title="Transfer all items into source"
+          onClick={handleTransferAllTargetItems}
+          disabled={! targetItems.length}>&laquo;</button>
+        <button 
+          title="Transfer items into Source"
+          onClick={handleTransferTargetItems}
+          disabled={! checkedTargetItems.length}>&lsaquo;</button>
+        <button 
+          title="Transfer items into Target"
+          onClick={handleTransferSourceItems}
+          disabled={! checkedSourceItems.length}>&rsaquo;</button>
+        <button 
+          title="Transfer all items into target"
+          onClick={handleTransferAllSourceItems}
+          disabled={! sourceItems.length}>&raquo;</button>
+      </div>
+      <CheckboxList 
+        type="target" 
+        data={targetItems} 
+        className={styles.target}
+        onChange={handleChange}
+        onSubmit={addItem} />
+    </div>
+  )
+}
